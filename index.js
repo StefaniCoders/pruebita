b4b0732da6d95643869c269dc80dc3d44bbede3f
stephanie <johanna.garatecar@gmail.com>
2017-06-26 13:21:57 -0700

    cree mi archivo para pruebita

---
+++ index.js
@@ -0,0 +1,15 @@
+var array=[4,4,5,12];
+var numero = 9;
+
+array.push(numero)
+document.getElementById("ejercicio1").innerHTML= array.reduce(function(primerValor,segundoValor,indice,vector){
+  return primerValor * segundoValor;
+});
+
+var assert = require("assert");
+
+function ejercicio1(array,numero){
+  var reduceArr = array.reduce(function(primerValor,segundoValor){
+    return
+  }
+}
